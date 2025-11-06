d060ba6bae1739bd624849c062ba268e440b60c6
imjel <maxineendieveri@gmail.com>
2025-11-06 16:51:43 -0500

    more stubs

--- app/components/ipod/body.tsx
+++ app/components/ipod/body.tsx
@@ -1,12 +1,33 @@
-import Wheel from "./wheel"
+import { useState } from "react";
+import Wheel from "./wheel";
 
-export default function Body () {
-    return (
-        <div className="flex w-full bg-blue-500 rounded-xl">
-            <div className="flex flex-col">
-                {/* screen goes here */}
-                <Wheel />
-            </div>
-        </div>
-    )
+export default function Body() {
+  const [currentScreen, setCurrentScreen] = useState("menu");
+  const [index, setIndex] = useState(0);
+  const [menuStack, setMenuStack] = useState(["home"]); // layers of routes
+
+  // const handleScroll = (index: number) => {
+  //     setIndex(prev => {
+  //         /** implement scroll logic */
+  //     })
+  // }
+
+  const handleSelect = () => {
+    // navigation
+  };
+
+  const handleMenu = () => {
+    // send user back to the previous menu
+    setMenuStack((prev) => prev.slice(0, -1));
+    setCurrentScreen(menuStack[menuStack.length - 2]);
+  };
+
+  return (
+    <div className="ipod-body flex w-full bg-blue-500 rounded-xl">
+      <div className="flex flex-col">
+        {/* screen goes here */}
+        <Wheel />
+      </div>
+    </div>
+  );
 }
--- app/components/ipod/wheel.tsx
+++ app/components/ipod/wheel.tsx
@@ -1,24 +1,39 @@
-import { FaPlay, FaFastForward, FaFastBackward, FaPause } from "react-icons/fa"
+/**
+ * wheel is input component
+ * detects user interactions & sends to controller:
+ *  - scrolling on outer wheel
+ *  - play/pause
+ *  - fast forward/backward
+ *  - select
+ *  - menu
+ */
+import { FaPlay, FaFastForward, FaFastBackward, FaPause } from "react-icons/fa";
 
-function handleClickMenu() {
-
+interface WheelProps {
+    onMenu
+    onSelect
+    onScroll
+    onPlay
+    onNext
+    onPrevious
 }
 
-export default function Wheel () {
-    return (
-        <div className="rounded-full bg-white border border-gray-50">
-            <button className="text-gray-100 text-md" onClick={handleClickMenu}>
-                MENU
-            </button>
-            <button className="text-gray-100 text-md">
-                {<FaFastForward size={24} className="text-gray-100"/>}
-            </button>
-            <button className="text-gray-100 text-md">
-                {<FaFastBackward size={24} className="text-gray-100"/>}
-            </button>
-            <button className="text-gray-100 text-md">
-                {<FaPause size={24} className="text-gray-100"/>}
-            </button>
-        </div>
-    )
+export default function Wheel() {
+
+  return (
+    <div className="rounded-full bg-white border border-gray-50">
+      <button className="text-gray-100 text-md">
+        MENU
+      </button>
+      <button className="text-gray-100 text-md">
+        {<FaFastForward size={24} className="text-gray-100" />}
+      </button>
+      <button className="text-gray-100 text-md">
+        {<FaFastBackward size={24} className="text-gray-100" />}
+      </button>
+      <button className="text-gray-100 text-md">
+        {<FaPause size={24} className="text-gray-100" />}
+      </button>
+    </div>
+  );
 }
