a773bfbc342ebfa926b049274da2a106759b7a3e
imjel <maxineendieveri@gmail.com>
2025-11-06 18:12:49 -0800

    battery

---
+++ app/components/ipod/screens/Battery.tsx
@@ -0,0 +1,5 @@
+export default function Battery() {
+  return (
+        <div className="battery-border-gradient battery-border-gradient-slate w-6 h-3 battery-gradient from-lime-300 to-lime-700 rounded-xs" />
+  );
+}
--- app/components/ipod/screens/ScreenHeader.tsx
+++ app/components/ipod/screens/ScreenHeader.tsx
@@ -1,7 +1,13 @@
+import Battery from "./Battery"
+
 export default function ScreenHeader() {
     return (
-        <div className="border-b border-[#237FAE] bg-screen shadow-inner w-full h-6 items-center text-center font-semibold">
-            <h1>iPod</h1>
+        <div className="flex flex-row border-b border-[#237FAE] bg-screen shadow-inner w-full h-6 items-center justify-between font-semibold">
+            <h1 className="flex-1 text-center ml-8">iPod</h1>
+            <div className="mr-1">
+            <Battery />
+            </div>
+            
         </div>
     )
 }
